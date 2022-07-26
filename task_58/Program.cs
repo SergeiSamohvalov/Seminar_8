6d65dc050cbb6cfb85e8d16d2427cf4445287e08
sergei sam <ssi1970@mail.ru>
2022-07-26 18:49:04 +0700

    задача 58

---
+++ task_58/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace Homework_8
+{
+    public class task_58
+    // Задайте две матрицы.
+    // Напишите программу, которая выведет матрицу произведения
+    // элементов двух предыдущих матриц.
+    {
+        static void Main(string[] args)
+        {
+            int[,] arrayFirst = new int[4, 4];
+            int[,] arraySecond = new int[4, 4];
+
+
+            FillarrayFirst(arrayFirst);
+            PrintArrayFirst(arrayFirst);
+            Console.WriteLine();
+            FillarraySecond(arraySecond);
+            PrintArraySecond(arraySecond);
+            Console.WriteLine();
+            MultiplicationArray(arrayFirst, arraySecond);
+
+
+
+        }
+
+        public static void FillarrayFirst(int[,] arr)
+
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = new Random().Next(1, 10);
+                }
+            }
+            return;
+        }
+
+        public static void FillarraySecond(int[,] arr)
+
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = new Random().Next(1, 10);
+                }
+            }
+            return;
+        }
+
+        public static void PrintArrayFirst(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"{arr[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintArraySecond(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"{arr[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void MultiplicationArray(int[,] arrayFirst, int[,] arraySecond)
+        {
+            var resultMultiplication = new int[4, 4];
+
+            for (int i = 0; i < arrayFirst.GetLength(0); i++)
+            {
+                for (int j = 0; j < arraySecond.GetLength(1); j++)
+                {
+                    resultMultiplication[i, j] += arrayFirst[i, j] * arraySecond[i, j];
+                    Console.Write($"{resultMultiplication[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            return;
+        }
+    }
+}
